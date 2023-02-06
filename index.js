f61188df32cc25da24f67544fc7b2d4c52fbf452
Cherise Foster <cherise.foster@outlook.com>
2023-02-06 10:40:49 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sara",
+    streetAddress: "MainStreet"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    newEmployee[key] = undefined;
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    employee[key] = undefined;
+    return employee;
+}
